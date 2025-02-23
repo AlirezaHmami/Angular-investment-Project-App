172767cec118b43a8b3c75a0e72dad1e9b8d212d
Alireza <alirezahamame@gmail.com>
2025-02-23 18:13:00 +0330

    improves

--- src/app/user-inputs/user-inputs.component.ts
+++ src/app/user-inputs/user-inputs.component.ts
@@ -25,8 +25,10 @@ export class UserInputsComponent {
       expectedReturn: this.enteredExpectedReturn(),
       duration: this.enteredDuration(),
     };
-
     this.onSubmit.emit(investmentData);
-
+    this.enteredInitialInvestment.set(0)
+    this.enteredAnnualInvestment.set(0)
+    this.enteredExpectedReturn.set(5);
+    this.enteredDuration.set(10);
   }
 }
